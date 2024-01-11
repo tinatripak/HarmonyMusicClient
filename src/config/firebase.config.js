78d860440df6284916ecfc634a8534f9b6850084
Kristina Tripak <tinatripak2002@gmail.com>
2024-01-11 01:30:42 -0500

    fixed bugs

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzUXFW-IVDU-yGHqFo3eGk8PdgaYYyemY",
-  authDomain: "musicapp-5ed7e.firebaseapp.com",
-  projectId: "musicapp-5ed7e",
-  storageBucket: "musicapp-5ed7e.appspot.com",
-  messagingSenderId: "314585488878",
-  appId: "1:314585488878:web:6d6db5eb041522511d42ff",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
