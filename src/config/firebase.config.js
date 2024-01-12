d1d702ccb51c3b2632cf039f861f8678ac57baa8
Kristina Tripak <tinatripak2002@gmail.com>
2024-01-11 20:23:53 -0500

    added console.log

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -10,6 +10,8 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+console.log(process.env)
+
 console.log(firebaseConfig)
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 const storage = getStorage(app);
